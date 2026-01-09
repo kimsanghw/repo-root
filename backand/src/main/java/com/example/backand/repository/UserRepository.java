94b124975dc5878c1c3c4c59c8ac1144aa0b6c2c
kimsanghw <tkdghk14@naver.com>
2026-01-09 16:20:24 +0900

    Delete backand directory (#3)

--- backand/src/main/java/com/example/backand/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.backand.repository;
-
-import com.example.backand.config.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> { }
