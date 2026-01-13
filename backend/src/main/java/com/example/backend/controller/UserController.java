857e18f89e246016a9564a5eeebf9c8d37b594d6
kimsanghw <tkdghk14@naver.com>
2026-01-13 11:51:56 +0900

    #1.첫번째 배포 (#4)

---
+++ backend/src/main/java/com/example/backend/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.backend.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.backend.config.entity.User;
+import com.example.backend.dto.CreateUserRequest;
+import com.example.backend.service.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService service;
+
+    @GetMapping
+    public List<User> list() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public User create(@RequestBody CreateUserRequest request) {
+        return service.create(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+
+}
---
+++ backend/src/main/java/com/example/backend/service/UserService.java
@@ -0,0 +1,32 @@
+package com.example.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import com.example.backend.config.entity.User;
+import com.example.backend.dto.CreateUserRequest;
+import com.example.backend.repository.UserRepository;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User create(CreateUserRequest request) {
+        User user = new User();
+        user.setName(request.getName());
+        user.setCity(request.getCity());
+        return repository.save(user);
+    }
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
+}
+
+
